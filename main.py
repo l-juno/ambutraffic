a3a043ae7f560124716ea7e30d2b9722572dbb44
Justin <justinlee13304@gmail.com>
2026-01-17 14:25:55 -0800

    1 intersection

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import pygame
 
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 700
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
 
-ROWS = 6
-COLS = 8
+ROWS = 1
+COLS = 1
 
-TOP_MARGIN = 10
-BOTTOM_MARGIN = 10
-LEFT_MARGIN = 10
-RIGHT_MARGIN = 10
+
+USABLE_WIDTH = SCREEN_WIDTH
+USABLE_HEIGHT = SCREEN_HEIGHT
+
 
 ROAD_COLOR = (50, 50, 50)
 BG_COLOR = (30, 30, 30)
+
+ROAD_THICKNESS = SCREEN_WIDTH // 4
+
+def draw_roads(screen):
+
+    road_rect = pygame.Rect(
+        (SCREEN_WIDTH - ROAD_THICKNESS) // 2,
+        0,
+        ROAD_THICKNESS,
+        SCREEN_HEIGHT
+    )
+    pygame.draw.rect(screen, ROAD_COLOR, road_rect)
+
+    road_rect = pygame.Rect(
+        0,
+        (SCREEN_HEIGHT - ROAD_THICKNESS) // 2,
+        SCREEN_WIDTH,
+        ROAD_THICKNESS
+    )
+    pygame.draw.rect(screen, ROAD_COLOR, road_rect)
+
+    
+
 
 
 
@@ -29,7 +52,9 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+        # --- draw ---
         screen.fill(BG_COLOR)
+        draw_roads(screen)
 
         pygame.display.flip()
         clock.tick(60)
